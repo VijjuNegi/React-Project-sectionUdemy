7d65459e0e53f8881bd399121d1f5dc715b96468
DESKTOP-E141O5H\Vijju <imvijju94negi@gmail.com>
2022-09-12 16:21:49 +0530

    Preparing The Project For The Next Steps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "../src/components/Css/App.css";
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-403a8-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something wrong! ");
@@ -37,6 +40,11 @@ function App() {
   useEffect(() => {
     moviesHandler();
   }, [moviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no Movies</p>;
 
   if (movies.length > 0) {
@@ -52,6 +60,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={moviesHandler}>Show</button>
       </section>
