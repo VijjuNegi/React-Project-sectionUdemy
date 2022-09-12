b40b2593660b1fc9d79baccaec14e7669619b4bc
DESKTOP-E141O5H\Vijju <imvijju94negi@gmail.com>
2022-09-12 14:25:31 +0530

    Using async  await

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import "../src/components/Css/App.css";
 
 function App() {
   const [movies, setMovie] = useState([]);
-  const moviesHandler = () => {};
-  fetch("https://swapi.dev/api/films/")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const transformedMovies = data.results.map((dataMovies) => {
-        return {
-          id: dataMovies.episode_id,
-          title: dataMovies.title,
-          openingText: dataMovies.opening_crawl,
-          releaseDate: dataMovies.release_date,
-        };
-      });
-      setMovie(transformedMovies);
+  async function moviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((dataMovies) => {
+      return {
+        id: dataMovies.episode_id,
+        title: dataMovies.title,
+        openingText: dataMovies.opening_crawl,
+        releaseDate: dataMovies.release_date,
+      };
     });
+    setMovie(transformedMovies);
+  }
   return (
     <React.Fragment>
       <section>
